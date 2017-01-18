51d11082c346432aafab63b526df4f75b65fc1ce
SeongkukPark <visualizerpsk@gmail.com>
2017-01-18 15:46:08 +0900

    v1.0.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,6 @@
 import '../css/style.css';
 import $ from 'jquery';
 
-const Please = require('pleasejs');
-
-let changeColor = () => {
-  $('#color').css('background-color', Please.make_color());
-}
-
 $('button').click(() => {
-  changeColor();
+  $('h1').html('hello world!');
 })
